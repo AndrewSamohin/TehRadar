9468b3a8d688c017ab0e009017dba2a6ae5980f4
Andrew Samohin <andreusamokhin@mail.ru>
2024-12-14 19:36:33 +0300

    first commit

---
+++ src/main/java/com/example/tehradar/TehradarApplication.java
@@ -0,0 +1,13 @@
+package com.example.tehradar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TehradarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TehradarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/tehradar/TehradarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tehradar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TehradarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
